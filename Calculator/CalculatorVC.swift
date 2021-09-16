8318350f766c38e436abdc0500910ab26736b9b2
Anatoliy Ostapenko <anatoliy.ostapenko@gmail.com>
2021-09-16 21:41:45 +0300

    design Story board

---
+++ Calculator/CalculatorVC.swift
@@ -0,0 +1,36 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by MacBook on 15.09.2021.
+//
+
+import UIKit
+
+class CalculatorVC: UIViewController {
+    
+    
+    @IBOutlet weak var displayLabel: UILabel!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    
+    @IBAction func actionButtonPressed(_ sender: UIButton) {
+        
+        
+    }
+    
+    
+    @IBAction func numberButtonPressed(_ sender: UIButton) {
+        
+        
+    }
+    
+    
+
+}
+
