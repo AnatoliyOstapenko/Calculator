51d6760bcfe3fef26417bc4e1687abed37decc9c
Anatoliy Ostapenko <anatoliy.ostapenko@gmail.com>
2021-09-29 17:46:17 +0300

    update model and controller pattern

--- Calculator/Controller/CalculatorVC.swift
+++ Calculator/Controller/CalculatorVC.swift
@@ -9,7 +9,21 @@ import UIKit
 
 class CalculatorVC: UIViewController {
     
-    var calculatingLogic = CalculatingLogic()
+    private var isFinishedTypingNumber: Bool = true
+    
+    private var displayValue: Double {
+        get {
+            guard let number = Double(displayLabel.text!) else {
+                fatalError("Cannot convert display label text to a Double.")
+            }
+            return number
+        }
+        set {
+            displayLabel.text = String(newValue)
+        }
+    }
+    
+    private var calculator = CalculatingLogic()
     
     @IBOutlet weak var displayLabel: UILabel!
     
@@ -22,21 +36,44 @@ class CalculatorVC: UIViewController {
     
     @IBAction func actionButtonPressed(_ sender: UIButton) {
         
-        guard let action = sender.currentTitle else { return }
+        //What should happen when a non-number button is pressed
         
-        // dispatch data to CalculatingLogic VC
-        calculatingLogic.action(action)
-        displayLabel.text = calculatingLogic.label
+        isFinishedTypingNumber = true
         
+        calculator.setNumber(displayValue)
+        
+        if let calcMethod = sender.currentTitle {
+ 
+            if let result = calculator.calculate(symbol: calcMethod) {
+                displayValue = result
+            }
+        }
     }
     
     
     @IBAction func numberButtonPressed(_ sender: UIButton) {
         
-        guard let number = sender.currentTitle else { return }
+        //What should happen when a number is entered into the keypad
         
-        calculatingLogic.number(number)
-        displayLabel.text = calculatingLogic.label
+        if let numValue = sender.currentTitle {
+            
+            if isFinishedTypingNumber {
+                displayLabel.text = numValue
+                isFinishedTypingNumber = false
+            } else {
+                
+                if numValue == "." {
+                    
+                    let isInt = floor(displayValue) == displayValue
+                    
+                    if !isInt {
+                        return
+                    }
+                }
+                displayLabel.text = displayLabel.text! + numValue
+            }
+        }
+
         
 
     }
--- Calculator/Model/CalculatingLogic.swift
+++ Calculator/Model/CalculatingLogic.swift
@@ -9,61 +9,52 @@ import Foundation
 
 struct CalculatingLogic {
     
-    var label: String = ""
-    var finish: Bool = true
-
+    private var number: Double?
     
-    var displayValue: Double {
-        get {
-            guard let number = Double(label) else { fatalError("error in displayValue") }
-            print("display value is \(number)")
-            return number
-        }
-        set {
-            if newValue == 0 {
-                label = String(format: "%.0f", newValue)
-                finish = true
-            } else {
-                label = String(newValue)
+    private var intermediateCalculation: (n1: Double, calcMethod: String)?
+    
+    mutating func setNumber(_ number: Double) {
+        self.number = number
+    }
+    
+    mutating func calculate(symbol: String) -> Double? {
+       
+        if let n = number {
+            switch symbol {
+            case "+/-":
+                return n * -1
+            case "AC":
+                return 0
+            case "%":
+                return n * 0.01
+            case "=":
+                return performTwoNumCalculation(n2: n)
+            default:
+                intermediateCalculation = (n1: n, calcMethod: symbol)
             }
         }
+        return nil
     }
     
-    mutating func action(_ action: String) {
+    private func performTwoNumCalculation(n2: Double) -> Double? {
         
-        switch action {
-        case "AC":
-            displayValue = 0
-        case "+/-":
-            displayValue *= -1
-        case "%":
-            displayValue = displayValue / 100
-        default:
-            displayValue = 0
+        if let n1 = intermediateCalculation?.n1,
+            let operation = intermediateCalculation?.calcMethod {
+            
+            switch operation {
+            case "+":
+                return n1 + n2
+            case "-":
+                return n1 - n2
+            case "*":
+                return n1 * n2
+            case "/":
+                return n1 / n2
+            default:
+                fatalError("The operation passed in does not match any of the cases.")
+            }
         }
-        
+        return nil
     }
     
-    mutating func number(_ number: String) {
-        
-        if finish == true {
-            label = number
-            finish = false
-        } else {
-            
-            // magic block of code:
-            if number == "." {
-                
-                // floor remove the fractional part and round down
-                let isInt = floor(displayValue) == displayValue
-                
-                if !isInt {
-                    return
-                }
-                
-            } // magic is over
-            
-            label += number
-        }
-    }
 }
